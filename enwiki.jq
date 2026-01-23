96765889e5b21be3ca33aea93c37b699fda506d9
MJ Suhonos <142849+mjsuhonos@users.noreply.github.com>
2026-01-23 14:33:55 -0500

    Clean up

--- enwiki.jq
+++
@@ -1,2 +0,0 @@
-select(.sitelinks.enwiki.title?)
-| "<http://www.wikidata.org/entity/\(.id)>\thttps://en.wikipedia.org/wiki/\(.sitelinks.enwiki.title | gsub(" "; "_"))"
--- pipeline.sh
+++ pipeline.sh
@@ -1,99 +1,142 @@
 #!/bin/zsh
+# Wikidata processing pipeline
+# Author: MJ Suhonos <mj@suhonos.ca>
+# Usage: ./pipeline.sh <source_dir> <working_dir> <class_names_file> <output_dir>
 
-# run in working directory
+set -euo pipefail
 
-#mkdir jena
-#mkdir nt
-#mkdir subjects
+# -----------------------
+# INPUT VARIABLES
+# -----------------------
+SOURCE_DIR=${1:-"../source"}
+WORK_DIR=${2:-"./working"}
+CLASS_NAMES_FILE=${3:-"$WORK_DIR/class_names.tsv"}
+OUTPUT_DIR=${4:-"$WORK_DIR/output"}
 
-# extract only the properties we're interested in
-gzcat ../source/wikidata-20251229-propdirect.nt | rg '/prop/direct/(P31|P279|P361)>' > wikicore-stage1-P31_P279_P361.nt
+NT_DIR="$WORK_DIR/nt"
+JENA_DIR="$WORK_DIR/jena"
+SUBJECTS_DIR="$WORK_DIR/subjects"
+QUERIES_DIR="$WORK_DIR/queries"
 
-# separate P31 statements out by type
-cd nt
-mawk -v OFS=' ' '
+RUN_DATE=$(date +%Y%m%d)
+COLLECTION_URI="https://wikicore.ca/$RUN_DATE"
+
+mkdir -p "$NT_DIR" "$SUBJECTS_DIR" "$JENA_DIR" "$OUTPUT_DIR"
+
+# -----------------------
+# 1. Extract core properties
+# -----------------------
+echo "Extracting P31 / P279 / P361…"
+
+CORE_PROPS_NT="$WORK_DIR/wikidata-core-props-P31-P279-P361.nt"
+
+gzcat "$SOURCE_DIR/wikidata-20251229-propdirect.nt.gz" \
+  | rg '/prop/direct/(P31|P279|P361)>' \
+  > "$CORE_PROPS_NT"
+
+# -----------------------
+# 2. Split by P31 class vs backbone
+# -----------------------
+echo "Partitioning P31 statements…"
+cd "$NT_DIR"
+
+mawk -v OFS=' ' -v CLASS_NAMES_FILE="$CLASS_NAMES_FILE" '
 function trim(s){ gsub(/^[ \t]+|[ \t]+$/, "", s); return s }
+
 BEGIN {
-    # Load class names TSV
-    while ((getline < "../../class_names.tsv") > 0)
+    while ((getline < CLASS_NAMES_FILE) > 0)
         class_names[trim($1)] = trim($2)
-    close("../../class_names.tsv")
+    close(CLASS_NAMES_FILE)
 }
+
 {
     subj=$1; pred=$2; obj=$3
     gsub(/[<>]/,"",subj); gsub(/[<>]/,"",pred); gsub(/[<>]/,"",obj)
 
     if (pred ~ /prop\/direct\/P31$/) {
-        # Extract QID quickly (everything after last /)
         qid = obj
         sub(".*/","",qid)
-
-        f = (qid in class_names) ? qid"_"class_names[qid]".nt" : "misc_p31_other.nt"
+        out_file = (qid in class_names)
+            ? qid "_" class_names[qid] "_instances.nt"
+            : "P31_other_instances.nt"
     } else {
-        f = "concept_backbone.nt"
+        out_file = "concept_backbone.nt"
     }
 
-    print $0 >> f
-    out[f]  # mark file handle
+    print $0 >> out_file
+    open[out_file]
 }
+
 END {
-    for (f in out) close(f)
+    for (f in open) close(f)
 }
-' ../wikicore-stage1-P31_P279_P361.nt
+' "$CORE_PROPS_NT"
 
-# extract all subjects and create QID lists
-# NB: these can be used as separate vocabularies in combination
-for a in Q*.nt; do ../../extract_unique_subjects.sh $a ../subjects/$a.tsv ; done
+# -----------------------
+# 3. Extract subject vocabularies
+# -----------------------
+echo "Extracting unique subjects…"
+for f in Q*_instances.nt; do
+    ../../extract_unique_subjects.sh "$f" "$SUBJECTS_DIR/${f%.nt}.subjects.tsv"
+done
 
+# -----------------------
+# 4. Load backbone into Jena
+# -----------------------
+echo "Loading backbone into Jena…"
+cd "$JENA_DIR"
+tdb2.tdbloader "$NT_DIR/concept_backbone.nt"
 
-#
-# ----------------FOLD ALONG THIS LINE -------------------
-#
+# -----------------------
+# 5. Materialize + export core concepts
+# -----------------------
+echo "Materializing graph and exporting core concepts…"
+tdb2.tdbupdate --update="$QUERIES_DIR/materialize_graph.rq"
 
+tdb2.tdbquery --query="$QUERIES_DIR/export.rq" --results=TSV \
+  > "$OUTPUT_DIR/core_concepts_raw.tsv"
 
+rg '<http://www.wikidata.org/entity/' "$OUTPUT_DIR/core_concepts_raw.tsv" \
+  | sort \
+  > "$WORK_DIR/core_concepts_qids.tsv"
 
-# Load structural graph into Jena
-cd ../jena
-tdb2.tdbloader --loc /Users/mjsuhonos/Desktop/Wikidata-vocabs/working.nosync/jena /Users/mjsuhonos/Desktop/Wikidata-vocabs/working.nosync/nt/concept_backbone.nt
+# -----------------------
+# 6. Remove core concepts from P31 instances
+# -----------------------
+echo "Filtering out core concepts…"
+cd "$WORK_DIR"
 
-# Calculate query optimizations
-tdb2.tdbupdate --loc /Users/mjsuhonos/Desktop/Wikidata-vocabs/working.nosync/jena --update=queries/materialize_graph.rq
+cat "$SUBJECTS_DIR"/*.subjects.tsv \
+  | sort -u \
+  | join -v 1 core_concepts_qids.tsv - \
+  > p31_noncore_qids.tsv
 
-# Export entities based on query
-tdb2.tdbquery --loc /Users/mjsuhonos/Desktop/Wikidata-vocabs/working.nosync/jena --query=queries/export.rq --results=TSV > core_vocab.tsv
+# -----------------------
+# 7. Generate SKOS triples
+# -----------------------
+echo "Generating SKOS triples…"
 
-# Clean up and sort entities for joining
-rg '<http://www.wikidata.org/entity' core_vocab.tsv| sort > ../wikicore-stage2_core_concepts.tsv
+sed -E 's|(.*)|\1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2004/02/skos/core#Concept> .|' \
+  p31_noncore_qids.tsv \
+  > skos_concepts.nt
 
+{
+  echo "<$COLLECTION_URI> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2004/02/skos/core#Collection> ."
+  sed -E "s|(.*)|<$COLLECTION_URI> <http://www.w3.org/2004/02/skos/core#member> \1 .|" \
+      p31_noncore_qids.tsv
+} > skos_collection.nt
 
+gzcat "$SOURCE_DIR/wikidata-20251229-skos-labels-en.nt.gz" \
+  | join - p31_noncore_qids.tsv \
+  > skos_labels_en.nt
 
+# -----------------------
+# 8. Export Turtle
+# -----------------------
+echo "Writing Turtle output…"
 
+cat skos_*.nt \
+  | rapper -i ntriples -o turtle - -I 'http://www.wikidata.org/entity/' \
+  > "$OUTPUT_DIR/wikicore-$RUN_DATE.ttl"
 
-
-#
-# ----------------DETACH HERE -------------------
-#
-
-# Combine excluded instances and remove them
-# TODO: optimize; uses 16GB RAM, maybe parallelize?
-
-# Usage: ./join_subjects_labels.sh subjects_unique.txt labels.nt output_joined.txt
-
-cd ..
-cat subjects/*.tsv | sort -u | join -v 1 wikicore-stage2_core_concepts.tsv - > wikicore-stage3-P31_removed.tsv
-
-# create skos:Concept statements from QIDs
-sed -E 's|(.*)|\1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2004/02/skos/core#Concept> .|' wikicore-stage3-P31_removed.tsv > wikicore-stage4-statements.nt
-
-# create skos:Collection statements from QIDs
-echo '<http://example.org/wikicore-20160122> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2004/02/skos/core#Collection> .' > wikicore-stage4-collection.nt
-sed -E 's|(.*)|<http://example.org/wikicore-20160122> <http://www.w3.org/2004/02/skos/core#member> \1 .|' wikicore-stage3-P31_removed.tsv >> wikicore-stage4-collection.nt
-
-# join with skos labels
-# NB: language can be selected here
-gzcat ../source/wikidata-20251229-skos-labels-en.nt.gz | join -  wikicore-stage3-P31_removed.tsv > wikicore-stage4-skos-en.nt
-
-# create turtle output ready to load
-cat wikicore-stage4* | rapper -i ntriples -o turtle - -I 'http://www.wikidata.org/entity/' > wikicore.ttl
-
-
+echo "Done → $OUTPUT_DIR/wikicore-$RUN_DATE.ttl"
